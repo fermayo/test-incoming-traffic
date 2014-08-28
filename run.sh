a16316f0fcd485e37e26129fd45352cc6798abf6
fermayo <fermayo@gmail.com>
2014-08-28 18:02:01 +0200

    added rate limiting

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -e
 while true; do
 	echo "=> Generating traffic..."
-	curl ${FILE:-"http://speedtest.reliableservers.com/100MBtest.bin"} -# > /dev/null
+	curl ${FILE:-"http://speedtest.reliableservers.com/100MBtest.bin"} -# --limit-rate ${RATE:-100k} > /dev/null
 done
